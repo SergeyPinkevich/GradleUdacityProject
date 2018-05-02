ac22d8ab6edb16339dc28eb75c448e35b29d5a20
Sergey Pinkevich <SAPinkevich.SBT@sberbank.ru>
2018-05-02 17:55:11 +0300

    add imports

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -10,8 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
-import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
@@ -49,13 +49,13 @@ public class MainActivityFragment extends Fragment implements AsyncTaskListener 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(), getString(R.string.loading_text), Toast.LENGTH_SHORT).show();
-                startAsyncTask();
                 if (mInterstitialAd.isLoaded()) {
                     mInterstitialAd.show();
                 } else {
                     Log.d("TAG", "The interstitial wasn't loaded yet.");
                 }
+                startAsyncTask();
+                Toast.makeText(getContext(), getString(R.string.loading_text), Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -63,7 +63,7 @@ public class MainActivityFragment extends Fragment implements AsyncTaskListener 
     }
 
     private void startAsyncTask() {
-        new EndpointsAsyncTask(this).execute(new Pair(this, "Manfred"));
+        new EndpointsAsyncTask(this).execute(new Pair(getContext(), "Manfred"));
     }
 
     @Override
